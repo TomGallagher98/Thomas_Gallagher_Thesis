56545d9b782d5b1694d975137b659fac6138b216
TomGallagher98 <79652005+TomGallagher98@users.noreply.github.com>
2023-02-03 17:35:14 +0100

    t01 cleaned code

--- Data/AFL_Stats/split_by_year.py
+++ Data/AFL_Stats/split_by_year.py
@@ -3,13 +3,8 @@ import time
 
 PATH = 'C:/Users/Craig/Documents/Thesis/Thomas_Gallagher_Thesis/Data/AFL_Stats_Sorted/'
 
-
-
 class Season():
-    def __init__(self, year) -> None:
-        self.Year = year
-        self.players = []
-        self.games = []
+    def __init__(self) -> None:
         self.Players = [[] for x in range(2012,2022)]
         self.Games = [[] for x in range(2012,2022)]
 
@@ -71,40 +66,7 @@ class Season():
             for player in gameList:
                 out = ','.join(str(player[1][x]) for x in fields)
                 file_output.writelines(out+'\n')
-            file_output.close()
-
-    # def split_into_years(self, stats, type):
-    #     print(time.time())
-    #     for data in stats.iterrows():
-    #         if data[1]['year'] == self.Year:
-    #             if type == "player":
-    #                 self.players.append(data)
-    #             else:
-    #                 self.games.append(data)
-            
-    #     print(time.time())
-
-    # def write_player_to_csv(self):
-    #     file_output = open(PATH+'Year/Players/'+str(self.Year)+'test.csv', 'w')
-    #     header = 'gameId,team,year,round,playerId,displayName,gameNumber,Disposals,Kicks,Marks,Handballs,Goals,Behinds,Hit Outs,Tackles,Rebounds,Inside 50s,Clearances,Clangers,Frees,Frees Against,Brownlow Votes,Contested Possessions,Uncontested Possessions,Contested Marks,Marks Inside 50,One Percenters,Bounces,Goal Assists,% Played,Subs'
-    #     file_output.writelines(header+'\n')
-        
-    #     fields = header.split(',')
-    #     for player in self.Players[0]:
-    #         out = ','.join(str(player[1][x]) for x in fields)
-    #         file_output.writelines(out+'\n')
-
-    # def write_game_to_csv(self):
-    #     file_output = open(PATH+'Year/Games/'+str(self.Year)+'.csv', 'w')
-    #     header = 'gameId,year,round,date,venue,startTime,attendance,homeTeam,homeTeamScore,awayTeam,awayTeamScore,rainfall'
-    #     file_output.writelines(header+'\n')
-        
-    #     fields = header.split(',')
-    #     for player in self.games:
-    #         out = ','.join(str(player[1][x]) for x in fields)
-    #         file_output.writelines(out+'\n')
-
-            
+            file_output.close()           
         
 stats = pd.read_csv(PATH + 'stats_sorted.csv')
 games = pd.read_csv(PATH + 'games_sorted.csv')
@@ -113,11 +75,9 @@ def show_dtypes(df):
     for index in range(len(df.dtypes)):
         print(f'{df.columns[index]} -> {df.dtypes[index]}' )
 
+t12 = Season()
 
-t12 = Season(2012)
-
-
-#t12.split_into_years_inner(stats, "player")
+t12.split_into_years_inner(stats, "player")
 t12.split_into_years_inner(games, "games")
-#t12.write_players_to_csv()
+t12.write_players_to_csv()
 t12.write_games_to_csv()
