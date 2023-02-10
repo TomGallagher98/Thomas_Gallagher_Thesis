8cc3729f2d78bb95536b46e2895c1368dec412c7
TomGallagher98 <79652005+TomGallagher98@users.noreply.github.com>
2023-02-10 17:16:00 +0100

    t03 updated distance function

--- Data/AFL_Stats/team_changes.py
+++ Data/AFL_Stats/team_changes.py
@@ -14,27 +14,7 @@ all_stats_raw = pd.read_csv(all_stats_path)
 
 def get_selected_team(gameId, team):
     team_l = all_stats_raw.query('gameId == @gameId and team == @team')
-
-
     return team_l
-
-    # Selected team can theoretically be inputted by the user
-    # For testing purposes it will be derived from the game data
-
-def player_prev_five(player, team = None):
-    #get stats from players previous 5 games
-    # pd.set_option('display.max_columns', None)
-    id = select_player(player, team)
-    print(id)
-    l_five = stats[(stats['playerId'] == id)]
-    return l_five.tail(5)
-
-def team_prev_five(team):
-    prev_games = games[(games['homeTeam'] == team) | (games['awayTeam'] == team)]
-    prev_games = prev_games.tail(5)
-    ids = [x for x in prev_games['gameId']]
-    print (ids)
-
 def team_similarity(s_team, prev_team, team):
     # Measures changes in selected team and previous team
     # HAMMING, Jaccard, Cosine (Will start with Hamming)
@@ -56,11 +36,23 @@ def team_similarity(s_team, prev_team, team):
     diff = sum(games_in) - sum(games_out)
     
     dist = (len(out_players))
-
-    evaluate_team_changes(in_players, out_players)
     # find changed players
-    return diff, dist
+    return dist, diff
+    # Selected team can theoretically be inputted by the user
+    # For testing purposes it will be derived from the game data
 
-def evaluate_team_changes(selected_team, previous_team):
-    print(selected_team)
-    print(sum(selected_team) - sum(previous_team))
+
+def player_prev_five(player, team = None):
+    #get stats from players previous 5 games
+    # pd.set_option('display.max_columns', None)
+    id = select_player(player, team)
+    print(id)
+    l_five = stats[(stats['playerId'] == id)]
+    return l_five.tail(5)
+
+def team_prev_five(team):
+    prev_games = games[(games['homeTeam'] == team) | (games['awayTeam'] == team)]
+    prev_games = prev_games.tail(5)
+    ids = [x for x in prev_games['gameId']]
+    print (ids)
+
