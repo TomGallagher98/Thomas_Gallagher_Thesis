212c84f74b85becf8e425d8cb708bb8593a03521
TomGallagher98 <79652005+TomGallagher98@users.noreply.github.com>
2023-03-15 20:52:00 +0100

    t07 added points and percentage

--- Data/AFL_Stats/Points_totals.py
+++ Data/AFL_Stats/Points_totals.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 def get_all_games(team: str, game_list: pd.DataFrame):
-    print(type(game_list))
     games = game_list.query('homeTeam == @team or awayTeam == @team')
     
     team_scores = []
@@ -21,44 +20,49 @@ def calculate_points(round: int, team_scores, opp_scores):
     team_for = sum(team_scores[:round - 1])
     team_against = sum(opp_scores[:round - 1])
 
-    print(team_for)
-    print(team_against)
-
 def calculate_points_row(row, team, team_scores, opp_scores):
-    print(row)
     if row['round'] == 1:
         return row
     if row.homeTeam == team:
         row['homeTeamPointsFor'] = sum(team_scores[:row['round'] - 1])
         row['homeTeamPointsAgainst'] =  sum(opp_scores[:row['round'] - 1])
+        row['homeTeamPercentage'] = (sum(team_scores[:row['round'] - 1]) / sum(opp_scores[:row['round'] - 1]))*100
         return row
     elif row.awayTeam == team:
         row['awayTeamPointsFor'] = sum(team_scores[:row['round'] - 1])
         row['awayTeamPointsAgainst'] =  sum(opp_scores[:row['round'] - 1])
+        row['awayTeamPercentage'] = (sum(team_scores[:row['round'] - 1]) / sum(opp_scores[:row['round'] - 1]))*100
         return row
     return row
+   
 
-    
-    
-
-teams = ['Hawthorn', 'Sydney']
+teams = ['Adelaide', 'Brisbane Lions','Carlton','Collingwood','Essendon','Fremantle','Geelong','Gold Coast', 'Greater Western Sydney',
+         'Hawthorn','Melbourne', 'North Melbourne','Port Adelaide','Richmond','St Kilda','Sydney','West Coast','Western Bulldogs']
+print(len(teams))
 def calc_season(season: pd.DataFrame):
     season['homeTeamPointsFor'] = 0
     season['homeTeamPointsAgainst'] = 0
+    season['homeTeamPercentage'] = 0
     season['awayTeamPointsFor'] = 0
     season['awayTeamPointsAgainst'] = 0
+    season['awayTeamPercentage'] = 0 
     for team in teams:
-        print(type(season))
         team_for, against = get_all_games(team, season)
         season = season.apply(calculate_points_row, args=(team, team_for, against), axis=1)
-        print(type(season))
-    print (season)
+    return season
     
-games_path = "C:/Users/Craig/Documents/Thesis/Thomas_Gallagher_Thesis/Data/AFL_Stats_sorted/Year/Games/2012.csv"
-games_2012_raw = pd.read_csv(games_path)
+games_path = "C:/Users/Craig/Documents/Thesis/Thomas_Gallagher_Thesis/Data/AFL_Stats_sorted/Year/Games/games_sorted.csv"
+games_raw = pd.read_csv(games_path)
 
-team_for, against = get_all_games('Collingwood', games_2012_raw)
+# team_for, against = get_all_games('Collingwood', games_2012_raw)
 
-calculate_points(5, team_for, against)
+# calculate_points(5, team_for, against)
 
-calc_season(games_2012_raw)
+games = calc_season(games_raw)
+games.to_csv(games_path, index=False)
+
+# for year in range(2012,2022):
+#     games_path = f"C:/Users/Craig/Documents/Thesis/Thomas_Gallagher_Thesis/Data/AFL_Stats_sorted/Year/Games/{year}.csv"
+#     games_raw = pd.read_csv(games_path)
+#     games = calc_season(games_2012_raw)
+#     games.to_csv(games_path, index=False)
